07fab9865bdfc32df2475f510f5768c1d8dedfb9
AdriManresa <adrimanresa62@gmail.com>
2024-10-23 17:51:50 -0300

    Creando modelos

--- src/Compra.java
+++ src/Compra.java
@@ -1,2 +1,24 @@
 public class Compra {
+    private double valor;
+    private String Descripcion;
+
+    public Compra(double valor, String descripcion) {
+        this.valor = valor;
+        Descripcion = descripcion;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+
+    public String getDescripcion() {
+        return Descripcion;
+    }
+
+    @Override
+    public String toString() {
+        return "Compra: " + "valor=" + valor +
+                ", Descripcion='" + Descripcion;
+    }
 }
--- src/TarjetaDeCredito.java
+++ src/TarjetaDeCredito.java
@@ -1,7 +1,36 @@
+import java.util.ArrayList;
 import java.util.List;
 
 public class TarjetaDeCredito {
     private double limite;
     private double saldo;
-    private List<Compra>listadeCompras;
+    private List<Compra> listadeCompras;
+
+    public TarjetaDeCredito(double limite) {
+        this.limite = limite;
+        this.saldo = limite;
+        this.listadeCompras = new ArrayList<>();
+    }
+
+    public boolean lanzarCompra(Compra compra) {
+        if (compra.getValor() <= this.saldo) {
+            this.saldo -= compra.getValor();
+            this.listadeCompras.add(compra);
+            return true;
+        }
+        return false;
+    }
+
+
+    public double getLimite() {
+        return limite;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public List<Compra> getListadeCompras() {
+        return listadeCompras;
+    }
 }
